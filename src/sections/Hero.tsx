10785f4dd18921e37b5000a12a606876c7e05824
Yanis <136376068+swiifu@users.noreply.github.com>
2025-03-13 02:10:24 +0000

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,10 @@
-import memojiImage from '@/assets/images/IMG_5970.png';
 import Image from "next/image";
 import ArrowDown from '@/assets/icons/arrow-down.svg';
 import grainImage from '@/assets/images/grain.jpg';
 import StarIcon from '@/assets/icons/star.svg';
 import {HeroOrbit} from "@/components/HeroOrbit";
 import SparkleIcon from '@/assets/icons/sparkle.svg';
+import memojiImage from '@/assets/images/IMG_5970.png';
 
 export const HeroSection = () => {
   return (
