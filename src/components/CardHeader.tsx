1986443ca31580cea2f3a34f0ee4f5ce59af8501
Yanis <136376068+swiifu@users.noreply.github.com>
2025-03-13 23:52:39 +0000

    Update CardHeader.tsx

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -1,5 +1,6 @@
 import StarIcon from "@/assets/icons/star.svg";
 import {twMerge} from "tailwind-merge";
+import { ReactNode } from "react";
 
 export const CardHeader = ({ title, description, className }: {
     title: string;
